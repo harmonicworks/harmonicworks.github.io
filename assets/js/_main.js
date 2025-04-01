5685fce601e327ac44091755f3aaec5c938546ed
Maurice <lesmusix@gmail.com>
2025-04-01 21:30:40 +0200

    Update _main.js
    Added code to open all links into new page.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -97,3 +97,14 @@ $(document).ready(function () {
   });
 
 });
+//Makes all links open in new page
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="http"]');
+  links.forEach(link => {
+    if (!link.href.includes(window.location.hostname)) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener");
+    }
+  });
+});
